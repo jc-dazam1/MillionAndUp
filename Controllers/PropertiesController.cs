d9aa00f826d488839b53e39a8371b8bd66a4de9c
Juan Daza <juan.daza@tory-tech.com>
2023-06-29 22:56:11 -0500

    Add Unit Tests files Postman

--- Controllers/PropertiesController.cs
+++ Controllers/PropertiesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -12,6 +13,7 @@ using MillionAndUp.Models;
 
 namespace MillionAndUp.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class PropertiesController : ControllerBase
